a2ef63aec7b7d670480434d640ba052a5586dcad
Szilágyi Zoltán <zoltan.richard.szilagyi@gmail.com>
2013-03-13 18:30:32 +0100

    add the base of the drop feature

--- js/formEditor/formEditor.js
+++ js/formEditor/formEditor.js
@@ -1,39 +1,41 @@
 (function($) {
-	
-var FormEditor = function(){
-	var self = this;
-	
-	var inputDescriptionButtonSelector = "div.sampleInputElement div.descriptionBoxButton";
-	
-	this.init = function(){
-		setSampleInputsDescriptionButtonEvents();
-		setFormInputEvents();
-	}
-	
-	setSampleInputsDescriptionButtonEvents = function(){
-		var buttons = $(inputDescriptionButtonSelector);
-		$(buttons).each(function(){
-			$(this).click(function(){
-				var sampleBox = $(this).next();
-				if(sampleBox.css('display') == 'none'){
-					sampleBox.fadeIn('slow');
-				}else{
-					sampleBox.fadeOut('fast');	
-				}
-				
-				
-			});
-		});
-	};
-	
-	setFormInputEvents = function(){
-		$( ".sampleInputElement" ).draggable({ revert: "valid" });
-	}
-	
-	
-};
 
-	
+	var FormEditor = function() {
+		var self = this;
+
+		var inputDescriptionButtonSelector = "div.sampleInputElement div.descriptionBoxButton";
+
+		this.init = function() {
+			setSampleInputsDescriptionButtonEvents();
+			setFormInputEvents();
+		}
+		setSampleInputsDescriptionButtonEvents = function() {
+			var buttons = $(inputDescriptionButtonSelector);
+			$(buttons).each(function() {
+				$(this).click(function() {
+					var sampleBox = $(this).next();
+					if (sampleBox.css('display') == 'none') {
+						sampleBox.fadeIn('slow');
+					} else {
+						sampleBox.fadeOut('fast');
+					}
+
+				});
+			});
+		};
+
+		setFormInputEvents = function() {
+			$(".sampleInputElement").draggable({
+				revert : true
+			});
+			$("#formEditor").droppable({
+				drop : function(event, ui) {
+					// TODO create input by dropped type
+				}
+			});
+		}
+	};
+
 	formEditor = new FormEditor();
-	formEditor.init();	
-})(jQuery);	
+	formEditor.init();
+})(jQuery);
