b4d191970b3eaea73eef073fdebd017f144e5e6e
Szilágyi Zoltán <zoltan.richard.szilagyi@gmail.com>
2013-05-30 20:09:21 +0200

    comments added

--- classes/posttype/CustomPostType.php
+++ classes/posttype/CustomPostType.php
@@ -11,22 +11,40 @@ abstract class CustomPostType{
 		$this->registerPostType();
 	}
 		
-	public function getPostType(){
+	/**
+     * Return with the post type descriptor data
+     */
+	public function getPostTypeData(){
 		return $this->postTypeData;
 	}
 	
+	/**
+     * Return with the id of the post type
+     */
 	public function getPostTypeId(){
 		return $this->postTypeId;
 	}
 	
+	/**
+     * Initializing the post type data, to create the custom post type
+     */
 	protected abstract function initPostTypeData();
 	
+	/**
+     * Add the meta boxes of the post type to the system
+     */
 	public abstract function addmetaBoxes();
 		
+	/**
+     * Register post type
+     */
 	final private function registerPostType(){
 		register_post_type($this->postTypeId, $this->postTypeData);
 	}
 	
+	/**
+     * Register meta box
+     */
 	final protected function registerMetaBox($metaBoxId, $label){
 		add_meta_box($metaBoxId, $label, 
 		array($this, $metaBoxId), $this->postTypeId );
